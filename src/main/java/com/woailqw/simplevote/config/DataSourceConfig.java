55e513cadb49de3339eb513e5aa97fb9c634b51d
jackpan <826647196@qq.com>
2020-08-10 15:41:28 +0800

    Data source configuration.

---
+++ src/main/java/com/woailqw/simplevote/config/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package com.woailqw.simplevote.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * data source config
+ *
+ * @author Jack Pan
+ * @version 1.00 2020-08-10
+ */
+@Configuration
+public class DataSourceConfig {
+
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource dataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+}
